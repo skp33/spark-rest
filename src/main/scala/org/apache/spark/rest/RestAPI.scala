64688b3f79ed3af9de428c44dfb384f3f8cc8e54
kprajapa <kaushal.prajapati@team.neustar>
2018-08-27 14:50:03 +0530

    Initial commit

--- src/main/scala/org/apache/spark/rest/RestAPI.scala
+++ src/main/scala/org/apache/spark/rest/RestAPI.scala
@@ -1,5 +1,8 @@
 package org.apache.spark.rest
 
+import org.apache.spark.{SparkContext, SparkException}
+import org.apache.spark.internal.Logging
+import org.apache.spark.ui.SparkUI
 import org.eclipse.jetty.servlet.{ServletContextHandler, ServletHolder}
 import org.glassfish.jersey.server.ServerProperties
 import org.glassfish.jersey.servlet.ServletContainer
@@ -7,7 +10,7 @@ import org.glassfish.jersey.servlet.ServletContainer
 /**
  * Created by KPrajapati on 8/25/2018.
  */
-object RestAPI {
+class RestAPI {
   def getServletHandler: ServletContextHandler = {
     val jerseyContext = new ServletContextHandler(ServletContextHandler.NO_SESSIONS)
     jerseyContext.setContextPath("/rest")
@@ -17,5 +20,25 @@ object RestAPI {
 
     jerseyContext.addServlet(holder, "/*")
     jerseyContext
+  }
+}
+
+object RestAPI extends Logging {
+  def getSparkUI(sparkContext: SparkContext): SparkUI = {
+    sparkContext.ui.getOrElse {
+      throw new SparkException("Parent SparkUI to attach this tab to not found!")
+    }
+  }
+
+  def apply(sparkContext: SparkContext): Unit = attach(sparkContext)
+
+  def attach(sparkContext: SparkContext): Unit = {
+    getSparkUI(sparkContext).attachHandler(new RestAPI().getServletHandler)
+    logInfo("Started rest-server")
+  }
+
+  def detach(sparkContext: SparkContext): Unit = {
+    getSparkUI(sparkContext).detachHandler(new RestAPI().getServletHandler)
+    logInfo("Stopped rest-server")
   }
 }
